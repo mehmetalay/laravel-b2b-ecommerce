24e680b335887e2deed1e0a1aa3b8e55e37d7f33
Mehmet Alay <mehmetaalay@gmail.com>
2026-05-13 11:56:52 +0300

    chore: prepare seeders for public repository

--- database/seeders/ThemeSettingSeeder.php
+++ database/seeders/ThemeSettingSeeder.php
@@ -15,8 +15,8 @@ class ThemeSettingSeeder extends Seeder
         $themeSettings = [
             [
                 'id' => 1,
-                'copyright' => '© 2026 TEST FIRMA - Tüm Hakları Saklıdır.',
-                'copyright_en' => '© 2026 TEST COMPANY - All rights reserved.',
+                'copyright' => '© 2026 FIRMA - Tüm Hakları Saklıdır.',
+                'copyright_en' => '© 2026 COMPANY - All rights reserved.',
                 'footer_about_us_text' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
                 'footer_about_us_text_en' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
                 'footer_social_title' => 'Bizi takip edin:',
@@ -26,7 +26,7 @@ class ThemeSettingSeeder extends Seeder
                 'pinterest' => 'https://tr.pinterest.com/',
                 'youtube' => 'https://www.youtube.com/?app=desktop&hl=tr',
                 'linkedin' => 'linkedin.com/home?originalSubdomain=tr',
-                'whatsapp' => '+90 500 000 00 00',
+                'whatsapp' => '5000000000',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
